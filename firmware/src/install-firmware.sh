ba9e5259ab178103afffc21661d94e33a92aef00
Michael Duren <michaeld@michaelduren.com>
2026-02-22 16:03:42 -0600

    feat:add firmware script for easy debugging & install

---
+++ firmware/src/install-firmware.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+echo "building"
+cd build/
+cmake ..
+make -j4
+cd ..
+
+echo "installing"
+picotool load build/pico_weather_watch.uf2 -f
+picotool reboot
+
+echo "connecting to serial output"
+sleep 2
+DEVICE=$(ls /dev/tty.usbmodem* 2>/dev/null | head -1)
+if [ -z "$DEVICE" ]; then
+    echo "no pico found"
+    exit 1
+fi
+echo "found pico at $DEVICE"
+minicom -b 115200 -D "$DEVICE"
